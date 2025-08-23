323b4144b2be12b6b91e35803a4c308ebad28783
wave1009 <wave.061009@gmail.com>
2025-08-24 03:57:34 +0900

    Feat: CORS 설정

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.routers import profile, graph, question, attempts, report, admin
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="Mentor AI")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 오리진 허용
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용
+    allow_headers=["*"],  # 모든 헤더 허용
+)
 
 # 라우터 등록
 app.include_router(profile.router, prefix="/profile", tags=["profile"])
